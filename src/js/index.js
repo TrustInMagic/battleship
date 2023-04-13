825b4d16d954f78650a4fc83c78ac9b7503ffa4c
Tim Ionescu <timionescu@gmail.com>
2023-04-14 00:01:38 +0300

    Add ship placement section (under construction)

--- src/js/index.js
+++ src/js/index.js
@@ -4,33 +4,50 @@ import { GameBoard } from './models/game-board';
 const playGame = (() => {
   const startForm = document.querySelector('.game-start');
 
-  startForm.addEventListener('submit', startNewGame);
+  startForm.addEventListener('submit', runShipPlacement);
 })();
 
-function startNewGame(e) {
+function runShipPlacement(e) {
   e.preventDefault();
   switchSection('ship-placement');
   transitionBackground();
 
-  const ships = {
-    Galleon: 5,
-    Frigate: 4,
-    Brigantine: 3,
-    Schooner: 2,
-    Sloop: 1,
-  };
   const nameInput = document.querySelector('.game-start input');
   const nameSpan = document.querySelector('.ship-to-place .player-name');
-  const shipSpan = document.querySelector('.ship-to-place .ship');
+  const axisButton = document.querySelector('.ship-placement button');
+  const axisDom = document.querySelector('.ship-placement .axis');
+
   nameSpan.textContent = nameInput.value;
-  shipSpan.textContent = Object.keys(ships)[0];
 
   const firstCaptain = Player(nameInput.value);
   const secondCaptain = Player('chat-GPT');
-  const boardObject = firstCaptain.playerBoard
-
+  const boardObject = firstCaptain.playerBoard;
   displayGameBoard(firstCaptain);
-  placeShipDom(boardObject, ships)
+  
+  let axis = 'horizontal';
+
+  axisButton.addEventListener('click', () => {
+    if (axis === 'horizontal') {
+      (axisDom.textContent = 'Vertical');
+      axis = 'vertical'
+    } else {
+      axisDom.textContent = 'Horizontal';
+      axis = 'horizontal'
+    }
+  });
+
+  const cellsDom = document.querySelectorAll('.board-cell');
+  let cellX;
+  let cellY;
+
+  cellsDom.forEach((cell) => {
+    cell.addEventListener('click', () => {
+      cellX = cell.getAttribute('data-x');
+      cellY = cell.getAttribute('data-y');
+      attemptShipPlacementDom('Galleon', axis)
+    });
+  });
+  
 }
 
 function displayGameBoard(player) {
@@ -49,19 +66,59 @@ function displayGameBoard(player) {
   }
 }
 
-function placeShipDom(boardObject, ships) {
+function findDomCellAtCoordinates(x, y) {
+
+}
+
+function attemptShipPlacementDom(shipType, axis) {
+  const ships = {
+    Galleon: 5,
+    Frigate: 4,
+    Brigantine: 3,
+    Schooner: 2,
+    Sloop: 1,
+  };
+  let ship;
+  let length;
+
+  switch (shipType) {
+    case 'Galleon':
+      ship = Object.keys(ships)[0];
+      length = ships[ship];
+      break;
+    case 'Frigate':
+      ship = Object.keys(ships)[1];
+      length = ships[ship];
+      break;
+    case 'Brigantine':
+      ship = Object.keys(ships)[2];
+      length = ships[ship];
+      break;
+    case 'Schooner':
+      ship = Object.keys(ships)[3];
+      length = ships[ship];
+      break;
+    case 'Sloop':
+      ship = Object.keys(ships)[4];
+      length = ships[ship];
+      break;
+  }
+
   const cellsDom = document.querySelectorAll('.board-cell');
   let cellX;
   let cellY;
 
-  console.log(cellsDom)
   cellsDom.forEach((cell) => {
     cell.addEventListener('mouseenter', () => {
       cellX = cell.getAttribute('data-x');
       cellY = cell.getAttribute('data-y');
-      console.log(cellX, cellY)
-    })
-  })
+
+      console.log(axis, length)
+    });
+  });
+
+  const shipSpan = document.querySelector('.ship-to-place .ship');
+  shipSpan.textContent = ship;
 }
 
 function switchSection(section) {
