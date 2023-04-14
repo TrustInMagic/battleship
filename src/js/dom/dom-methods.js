89524c2c31a2a0d99377d6236506bffefd995dba
Tim Ionescu <timionescu@gmail.com>
2023-04-14 17:35:45 +0300

    Split and refactor attemptShipPlacementDom()

---
+++ src/js/dom/dom-methods.js
@@ -0,0 +1,142 @@
+export function displayGameBoard(player) {
+  const board = player.playerBoard.returnBoard();
+  const gameBoardDom = document.querySelector('.ship-placement .game-board');
+
+  for (let row of board) {
+    for (let cellArr of row) {
+      const cell = cellArr[0];
+      const cellDom = document.createElement('div');
+      cellDom.classList.add('board-cell');
+      cellDom.setAttribute('data-x', cell.x);
+      cellDom.setAttribute('data-y', cell.y);
+      gameBoardDom.appendChild(cellDom);
+    }
+  }
+}
+
+function clearDomCellInvalidity(cell) {
+  cell.addEventListener('mouseleave', () => {
+    cell.classList.remove('invalid-location');
+  });
+}
+
+function clearDomCellsCustomColor(cells) {
+  cells.forEach((cell) => cell.classList.remove('place-ship'));
+}
+
+function findDomCellAtCoordinates(x, y) {
+  const cellsDom = document.querySelectorAll('.board-cell');
+  let searchedCell;
+
+  cellsDom.forEach((cell) => {
+    const cellX = cell.getAttribute('data-x');
+    const cellY = cell.getAttribute('data-y');
+    if (Number(cellX) === x && Number(cellY) === y) searchedCell = cell;
+  });
+
+  return searchedCell;
+}
+
+function getShipName(shipType) {
+  const ships = {
+    Galleon: 5,
+    Frigate: 4,
+    Brigantine: 3,
+    Schooner: 2,
+    Sloop: 1,
+  };
+  let ship;
+  let length;
+
+  switch (shipType) {
+    case 'Galleon':
+      ship = Object.keys(ships)[0];
+      length = ships[ship];
+      break;
+    case 'Frigate':
+      ship = Object.keys(ships)[1];
+      length = ships[ship];
+      break;
+    case 'Brigantine':
+      ship = Object.keys(ships)[2];
+      length = ships[ship];
+      break;
+    case 'Schooner':
+      ship = Object.keys(ships)[3];
+      length = ships[ship];
+      break;
+    case 'Sloop':
+      ship = Object.keys(ships)[4];
+      length = ships[ship];
+      break;
+  }
+  return { ship, length };
+}
+export function attemptShipPlacementDom(shipType, axis, cell, board) {
+  const shipData = getShipName(shipType);
+  const shipSpan = document.querySelector('.ship-to-place .ship');
+  shipSpan.textContent = shipData.ship;
+  const length = shipData.length;
+
+  const shipHead = [cell.x, cell.y];
+  let shipTailX;
+  let shipTailY;
+  let direction;
+
+  if (axis === 'horizontal') {
+    shipTailX = shipHead[0] + length - 1;
+    shipTailY = shipHead[1];
+    direction = 'horizontal';
+  } else if (axis === 'vertical') {
+    shipTailX = shipHead[0];
+    shipTailY = shipHead[1] + length - 1;
+    direction = 'vertical';
+  }
+
+  const shipTail = [shipTailX, shipTailY];
+  const restShipCells = [];
+  const shipHeadDom = [findDomCellAtCoordinates(...shipHead)];
+  const shipTailDom = [findDomCellAtCoordinates(...shipTail)];
+  const restShipDom = [];
+
+  if (!board.checkBoatPlacementValidity(shipHead, shipTail)) {
+    shipHeadDom[0].classList.add('invalid-location');
+    clearDomCellInvalidity(shipHeadDom[0]);
+  } else {
+    const missingCells = board.findMissingBoatCells(
+      shipHead,
+      shipTail,
+      length,
+      direction
+    );
+    restShipCells.push(...missingCells);
+
+    restShipCells.forEach((cell) => {
+      if (cell === undefined) return;
+      restShipDom.push(findDomCellAtCoordinates(cell.x, cell.y));
+    });
+
+    const allDomShipCells = shipHeadDom.concat(shipTailDom).concat(restShipDom);
+    allDomShipCells.forEach((cell) => {
+      cell.classList.add('place-ship');
+      cell.addEventListener('mouseleave', () =>
+        clearDomCellsCustomColor(allDomShipCells)
+      );
+    });
+  }
+}
+
+export function switchSection(section) {
+  const landingSection = document.querySelector('.landing');
+  const shipPlacement = document.querySelector('.ship-placement');
+
+  if (section === 'ship-placement') {
+    landingSection.style.cssText = 'display: none';
+    shipPlacement.style.cssText = 'display: flex';
+  }
+}
+
+export function transitionBackground() {
+  const shipPlacement = document.querySelector('.ship-placement');
+  shipPlacement.classList.add('background-swap');
+}
